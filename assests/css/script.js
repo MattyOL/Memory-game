a6e16dea08494bf66c84552385922ffce0efe2fb
MattyOL <mattyoleary1997@gmail.com>
2022-11-11 17:20:50 +0000

    adding image to script

--- assests/css/script.js
+++ assests/css/script.js
@@ -12,14 +12,14 @@ let firstCard = false;
 let secondCard = false;
 
 const items = [
-    { name: "", image: ""},
-    { name: "", image: ""},
-    { name: "", image: ""},
-    { name: "", image: ""},
-    { name: "", image: ""},
-    { name: "", image: ""},
-    { name: "", image: ""},
-    { name: "", image: ""},
-    { name: "", image: ""},
-    { name: "", image: ""},
+    { name: "blue", image: "blue.png"},
+    { name: "burdundy", image: "burgundy.png"},
+    { name: "darkred", image: "darkred.png"},
+    { name: "green", image: "green.png"},
+    { name: "grey", image: "grey.png"},
+    { name: "lightoj", image: "lightoj.png"},
+    { name: "purple", image: "purple.png"},
+    { name: "springgreen", image: "springgreen.png"},
+    { name: "tan", image: "tan.png"},
+    { name: "teal", image: "teal.png"},
 ];
