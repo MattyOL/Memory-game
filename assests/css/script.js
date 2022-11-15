6247f2fc9e64a022fbd73b1dffb4df8439b6ebb6
MattyOL <mattyoleary1997@gmail.com>
2022-11-15 17:02:30 +0000

    adding js for timer / moves

--- assests/css/script.js
+++ assests/css/script.js
@@ -30,3 +30,53 @@ let seconds = 0;
 let moveCount = 0;
     winCount = 0;  
 
+//For timer
+const timeGenerator = () => {
+    seconds += 1;
+    //minutes logic
+    if (seconds >= 60) {
+      minutes += 1;
+      seconds = 0;
+    }
+    //format time before displaying
+    let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+  };
+  //For calculating moves
+const movesCounter = () => {
+    movesCount += 1;
+    moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+  };
+  //Pick random objects from the items array
+const generateRandom = (size = 4) => {
+    //temporary array
+    let tempArray = [...items];
+    //initializes cardValues array
+    let cardValues = [];
+    //size should be double (4*4 matrix)/2 since pairs of objects would exist
+    size = (size * size) / 2;
+    //Random object selection
+    for (let i = 0; i < size; i++) {
+      const randomIndex = Math.floor(Math.random() * tempArray.length);
+      cardValues.push(tempArray[randomIndex]);
+      //once selected remove the object from temp array
+      tempArray.splice(randomIndex, 1);
+    }
+    return cardValues;
+  };
+const matrixGenerator = (cardValues, size = 4) => {
+    gameContainer.innerHTML = "";
+    cardValues = [...cardValues, ...cardValues];
+    // shuffle
+    cardValues.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < size * size; i++) {
+      
+      gameContainer.innerHTML += `
+       <div class="card-container" data-card-value="${cardValues[i].name}">
+          <div class="card-before">?</div>
+          <div class="card-after">
+          <img src="${cardValues[i].image}" class="image"/></div>
+       </div>
+       `;
+    } 
